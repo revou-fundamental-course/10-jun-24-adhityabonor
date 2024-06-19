61774312578ed48082f8f9ebe78b493429094f39
adhityabonor <bonoradhitya@gmail.com>
2024-06-20 00:00:21 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// Ini Javascript
+// Fungsi untuk konversi Celcius ke Fahrenheit
+function convertCelsiusToFahrenheit() {
+    var celsius = document.getElementById('inputCelcius').value;
+    if (isNaN(celsius) || celsius.trim() === '') {
+        document.getElementById('outputFahrenheit').value = "Error";
+        document.getElementById('formula').value = "Error";
+        alert('Input invalid (Cek kembali input anda)');
+        return;
+    }
+    celsius = parseFloat(celsius);
+    var fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById('outputFahrenheit').value = fahrenheit.toFixed(2);
+    document.getElementById('formula').value = `(${celsius} °C × 9/5) + 32 = ${fahrenheit.toFixed(2)} °F`;
+}
+
+// Fungsi untuk konversi Fahrenheit ke Celcius
+function convertFahrenheitToCelsius() {
+    var fahrenheit = document.getElementById('outputFahrenheit').value;
+    if (isNaN(fahrenheit) || fahrenheit.trim() === '') {
+        document.getElementById('inputCelcius').value = "Error";
+        document.getElementById('formula').value = "Error";
+        alert('Input invalid (Cek kembali input anda)');
+        return;
+    }
+    fahrenheit = parseFloat(fahrenheit);
+    var celsius = (fahrenheit - 32) * 5/9;
+    document.getElementById('inputCelcius').value = celsius.toFixed(2);
+    document.getElementById('formula').value = `(${fahrenheit} °F - 32) × 5/9 = ${celsius.toFixed(2)} °C`;
+}
+
+// Fungsi untuk mereset nilai semua textbox
+function resetValues() {
+    document.getElementById('inputCelcius').value = '';
+    document.getElementById('outputFahrenheit').value = '';
+    document.getElementById('formula').value = '';
+}
+
+// Tambahkan event listener ke tombol konversi Celsius ke Fahrenheit
+document.getElementById('convertButton').addEventListener('click', convertCelsiusToFahrenheit);
+
+// Tambahkan event listener ke tombol konversi Fahrenheit ke Celsius
+document.getElementById('convertReverse').addEventListener('click', convertFahrenheitToCelsius);
+
+// Tambahkan event listener ke tombol reset
+document.getElementById('resetButton').addEventListener('click', resetValues);
+
+//Dibuat oleh Raffy Ahmad Jaliyyan//
+//SAYA SANGAT MENGHARGAI ANDA JIKA ANDA TIDAK COPY-PASTE PROJECT SAYA TANPA IZIN//
